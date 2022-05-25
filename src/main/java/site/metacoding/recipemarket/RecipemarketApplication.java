b937fd94d54a1cf0290a1b6bbeaf8158333b7381
tjdgus7562 <tjdgus7562@naver.com>
2022-05-25 16:02:03 +0900

    [정성현] initial commit

---
+++ src/main/java/site/metacoding/recipemarket/RecipemarketApplication.java
@@ -0,0 +1,13 @@
+package site.metacoding.recipemarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipemarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipemarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/site/metacoding/recipemarket/RecipemarketApplicationTests.java
@@ -0,0 +1,13 @@
+package site.metacoding.recipemarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipemarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
