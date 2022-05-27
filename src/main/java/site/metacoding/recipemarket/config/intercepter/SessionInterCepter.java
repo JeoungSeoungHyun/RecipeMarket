d71f880ecbe61c15ff08b799a2a0bfea356737b3
seongyung <282203@naver.com>
2022-05-27 18:40:12 +0900

    [이선경] Security config, handler, exdeption 세팅 완료

--- src/main/java/site/metacoding/recipemarket/config/intercepter/SessionInterCepter.java
+++
@@ -1,5 +0,0 @@
-package site.metacoding.recipemarket.config.intercepter;
-
-public class SessionInterCepter {
-
-}
--- src/main/java/site/metacoding/recipemarket/handler/LoginFailureHandler.java
+++ src/main/java/site/metacoding/recipemarket/handler/LoginFailureHandler.java
@@ -1,5 +1,22 @@
 package site.metacoding.recipemarket.handler;
 
-public class LoginFailureHandler {
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+
+public class LoginFailureHandler implements AuthenticationFailureHandler {
+
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
+            AuthenticationException exception) throws IOException, ServletException {
+
+                System.out.println("exception : " + exception.getMessage());
+                response.sendRedirect("/login"); // login 프로세스를 탄다.
+    }
 
 }
