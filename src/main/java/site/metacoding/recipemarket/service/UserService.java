43ebf5ed9df2a5fb0675df606de7b56154003b75
gih1214 <gih1214@hanmail.net>
2022-05-29 21:40:01 +0900

    [이선경] 회원가입 구현중...(프로필 이미지 등록 미완)

--- src/main/java/site/metacoding/recipemarket/service/UserService.java
+++ src/main/java/site/metacoding/recipemarket/service/UserService.java
@@ -1,11 +1,37 @@
 package site.metacoding.recipemarket.service;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import lombok.RequiredArgsConstructor;
+import site.metacoding.recipemarket.domain.user.User;
+import site.metacoding.recipemarket.domain.user.UserRepository;
 
 @RequiredArgsConstructor
 @Service
 public class UserService {
 
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    // 파일 저장 경로
+    @Value("${file.path}")
+    private String uploadFolder;
+
+    // !!!!이미지 오류 해결 필요!!!!
+    @Transactional // 트랜잭션 처리로 오류 발생 시 롤백
+    public void 회원가입(User user) {
+        // 비밀번호 해시 암호화
+        String rawPassword = user.getPassword(); // 입력 받은 원본 패스워드
+        String encPassword = bCryptPasswordEncoder.encode(rawPassword); // 원본 패스워드에 해쉬 알고리즘 적용
+        user.setPassword(encPassword); // 해시화된 패스워드로 값 변경
+
+        // profileImg UUID로 변경하여 경로를 저장하기
+        // String profileImg = UtilFileUpload.write(uploadFolder, joinReqDto.getFile());
+        // User user = joinReqDto.toEntity(profileImg);
+
+        userRepository.save(user);
+    }
 }
--- src/main/java/site/metacoding/recipemarket/web/dto/user/JoinReqDto.java
+++ src/main/java/site/metacoding/recipemarket/web/dto/user/JoinReqDto.java
@@ -1,5 +1,56 @@
 package site.metacoding.recipemarket.web.dto.user;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import site.metacoding.recipemarket.domain.user.User;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data // Getter, Setter, toString
 public class JoinReqDto {
 
+    // 영어 대소문자, 숫자만 허용
+    @Pattern(regexp = "[a-zA-Z1-9]{4,20}", message = "아이디는 한글, 공백, 특수문자가 들어갈 수 없습니다.")
+    @Size(min = 4, max = 20, message = "아이디는 최소 4자리, 최대 20자리까지 입력 가능합니다.")
+    @NotBlank // @NotNull, @NotEmpty 두개의 조합
+    private String username;
+
+    // 한글, 영어 대소문자, 숫자만 허용
+    @Pattern(regexp = "[ㄱ-ㅎ가-힣a-zA-Z1-9]{1,12}", message = "닉네임은 공백, 특수문자가 들어갈 수 없습니다.")
+    @Size(min = 1, max = 12, message = "닉네임은 최소 1자리, 최대 12자리까지 입력 가능합니다.")
+    @NotBlank
+    private String nickname;
+
+    // 영어 대소문자, 숫자, 특수문자만 허용
+    @Pattern(regexp = "[a-zA-Z1-9~!@#$%^&*()_+|<>?:{}]{8,16}", message = "비밀번호는 한글, 공백이 들어갈 수 없습니다.")
+    @Size(min = 8, max = 16, message = "비밀번호는 최소 8자리, 최대 16자리까지 입력 가능합니다.")
+    @NotBlank
+    private String password;
+
+    @Size(min = 1, max = 60, message = "이메일은 최소 1자리, 최대 60자리까지 입력 가능합니다.")
+    @NotBlank
+    @Email
+    private String email;
+
+    // private MultipartFile file;
+
+    private String profileImg; // 프로필 사진 할 때 지우고,
+
+    public User toEntity() { // 여기 매개변수로 받기
+        User user = new User();
+        user.setUsername(username);
+        user.setNickname(nickname);
+        user.setPassword(password);
+        user.setEmail(email);
+        user.setProfileImg(profileImg);
+        return user;
+    }
 }
