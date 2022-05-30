8610c651b0a4178de7b21bede7330a80c2786167
AR <aki1013@naver.com>
2022-05-30 14:20:00 +0900

    [고아라] 공통css 분류

--- src/main/java/site/metacoding/recipemarket/web/UserController.java
+++ src/main/java/site/metacoding/recipemarket/web/UserController.java
@@ -1,5 +1,14 @@
 package site.metacoding.recipemarket.web;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class UserController {
+
+    @GetMapping("main")
+    public String main() {
+        return "/main";
+    }
 
 }
