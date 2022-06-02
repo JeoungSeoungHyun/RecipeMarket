fd7aa9822c963f93f45f7f364ef1a14016bd2257
AR <aki1013@naver.com>
2022-06-02 22:57:16 +0900

    [고아라] 컨트롤러 정리

--- src/main/java/site/metacoding/recipemarket/web/UserController.java
+++ src/main/java/site/metacoding/recipemarket/web/UserController.java
@@ -6,29 +6,4 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class UserController {
 
-    @GetMapping("main")
-    public String main() {
-        return "/main";
-    }
-
-    @GetMapping("join-form")
-    public String join() {
-        return "/user/joinForm";
-    }
-
-    @GetMapping("login-form")
-    public String login() {
-        return "/user/loginForm";
-    }
-
-    @GetMapping("find-username-form")
-    public String findUsernameForm() {
-        return "/user/findUsernameForm";
-    }
-
-    @GetMapping("find-password-form")
-    public String findPasswordForm() {
-        return "/user/findPasswordForm";
-    }
-
 }
