0a9cf82e540c8fa22ded02bf668879e247e1f39f
gih1214 <gih1214@hanmail.net>
2022-06-12 22:25:45 +0900

    [구아현] 댓글 구현중...

--- src/main/java/site/metacoding/recipemarket/service/CommentService.java
+++ src/main/java/site/metacoding/recipemarket/service/CommentService.java
@@ -1,5 +1,34 @@
 package site.metacoding.recipemarket.service;
 
-public class CommentService {
+import java.util.Optional;
 
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import lombok.RequiredArgsConstructor;
+import site.metacoding.recipemarket.domain.comment.Comment;
+import site.metacoding.recipemarket.domain.comment.CommentRepository;
+import site.metacoding.recipemarket.domain.post.Post;
+import site.metacoding.recipemarket.domain.post.PostRepository;
+
+@RequiredArgsConstructor
+@Service
+public class CommentService {
+    private final CommentRepository commentRepository;
+    private final PostRepository postRepository;
+
+    @Transactional
+    public void 댓글쓰기(Comment comment, Integer postId) {
+
+        Optional<Post> postOp = postRepository.findById(postId);
+
+        if (postOp.isPresent()) { // 게시글 번호가 있으면
+            Post postEntity = postOp.get(); // entity에 담아서
+            comment.setPost(postEntity); // comment의 post에 담기
+        } else {
+            throw new RuntimeException("없는 게시글에 댓글을 작성할 수 없습니다");
+        }
+
+        commentRepository.save(comment); // 댓글 insert
+    }
 }
--- src/main/java/site/metacoding/recipemarket/web/CommentController.java
+++ src/main/java/site/metacoding/recipemarket/web/CommentController.java
@@ -1,5 +1,38 @@
 package site.metacoding.recipemarket.web;
 
-public class CommentController {
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import lombok.RequiredArgsConstructor;
+import site.metacoding.recipemarket.config.auth.LoginUser;
+import site.metacoding.recipemarket.domain.comment.Comment;
+import site.metacoding.recipemarket.domain.user.User;
+import site.metacoding.recipemarket.service.CommentService;
+
+@RequiredArgsConstructor
+@Controller
+public class CommentController {
+    private final CommentService commentService;
+
+    @GetMapping("/post")
+    public String list() {
+        return "/post/list";
+    }
+
+    @GetMapping("/post/{postId}")
+    public String detail() {
+        return "/post/detail";
+    }
+
+    @PostMapping("/s/post/{postId}/comment")
+    public String write(@PathVariable Integer postId, Comment comment, @AuthenticationPrincipal LoginUser loginUser) {
+
+        User principal = loginUser.getUser(); // 시큐리티 세션에 있는 유저정보를
+        comment.setUser(principal); // comment의 유저에 담아서
+        commentService.댓글쓰기(comment, postId); // 서비스로 보내기
+        return "redirect:/post/" + postId;
+    }
 }
