853e67152489a7567487221657513d71843a2950
AR <aki1013@naver.com>
2022-06-13 22:52:25 +0900

    [고아라] 상세페이지 완료

--- src/main/java/site/metacoding/recipemarket/web/UserController.java
+++ src/main/java/site/metacoding/recipemarket/web/UserController.java
@@ -5,49 +5,5 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UserController {
-    @GetMapping("main")
-    public String main() {
-        return "/main";
-    }
-
-    @GetMapping("join-form")
-    public String join() {
-        return "/user/joinForm";
-    }
-
-    @GetMapping("login-form")
-    public String login() {
-        return "/user/loginForm";
-    }
-
-    @GetMapping("find-username-form")
-    public String findUsernameForm() {
-        return "/user/findUsernameForm";
-    }
-
-    @GetMapping("reset-password-form")
-    public String resetPasswordForm() {
-        return "/user/resetPasswordForm";
-    }
-
-    @GetMapping("update-form")
-    public String updateForm() {
-        return "/user/updateForm";
-    }
-
-    @GetMapping("mypage-form")
-    public String myPage() {
-        return "/user/myPageForm";
-    }
-
-    @GetMapping("list-form")
-    public String list() {
-        return "/post/listForm";
-    }
-
-    @GetMapping("detail-form")
-    public String detail() {
-        return "/post/detailForm";
-    }
 
 }
