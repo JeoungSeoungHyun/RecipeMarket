1b08d943d4ab851636422676737eb7adbe9ef1c8
AHyun <97711630+gih1214@users.noreply.github.com>
2022-06-15 19:12:30 +0900

    Post comment (#10)
    * [구아현] 댓글 구현중...
    
    * [구아현] 댓글 등록,조회,삭제 구현 완료
    
    * [구아현] 댓글 수정 구현
    
    * [구아현] 쿠키로직 수정
    
    * [구아현] 쿠키로직 오류 해결
    
    * [구아현] 수정완료

--- src/main/java/site/metacoding/recipemarket/service/CommentService.java
+++ src/main/java/site/metacoding/recipemarket/service/CommentService.java
@@ -1,5 +1,67 @@
 package site.metacoding.recipemarket.service;
 
-public class CommentService {
+import java.util.Optional;
 
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import lombok.RequiredArgsConstructor;
+import site.metacoding.recipemarket.domain.comment.Comment;
+import site.metacoding.recipemarket.domain.comment.CommentRepository;
+import site.metacoding.recipemarket.domain.post.Post;
+import site.metacoding.recipemarket.domain.post.PostRepository;
+import site.metacoding.recipemarket.domain.user.User;
+import site.metacoding.recipemarket.handler.ex.CustomException;
+import site.metacoding.recipemarket.web.dto.comment.CommentReqDto;
+
+@RequiredArgsConstructor
+@Service
+public class CommentService {
+    private final CommentRepository commentRepository;
+    private final PostRepository postRepository;
+
+    @Transactional
+    public void 댓글수정(Integer id, User principal, CommentReqDto dto) {
+        Optional<Comment> commentOp = commentRepository.findById(id);
+
+        if (commentOp.isPresent()) { // id가 존재하면
+            if (principal.getId() != commentOp.get().getUser().getId()) { // 세션 id와 디비에 있는 id가 다르면
+                throw new CustomException("수정 권한이 없습니다."); // 권한 X
+            }
+        } else { // id가 존재하지 않으면 예외처리
+            throw new RuntimeException("해당 댓글이 없습니다");
+        }
+        Comment commentEntity = commentOp.get();
+        commentEntity.setContent(dto.toEntity().getContent()); // 영속화된 오브젝트 수정
+    }
+
+    @Transactional
+    public void 댓글삭제(Integer id, User principal) {
+        Optional<Comment> commentOp = commentRepository.findById(id); // 댓글 id 찾아서
+
+        if (commentOp.isPresent()) { // id가 존재하면
+            if (principal.getId() != commentOp.get().getUser().getId()) { // 세션 id와 디비에 있는 id가 다르면
+                throw new CustomException("삭제 권한이 없습니다."); // 권한 X
+            }
+        } else { // id가 존재하지 않으면 예외처리
+            throw new RuntimeException("해당 댓글이 없습니다");
+        }
+        // 세션 id와 디비 id가 같으면 댓글 삭제
+        commentRepository.deleteById(id);
+    }
+
+    @Transactional
+    public void 댓글쓰기(Comment comment, Integer postId) {
+
+        Optional<Post> postOp = postRepository.findById(postId);
+
+        if (postOp.isPresent()) { // 게시글 번호가 있으면
+            Post postEntity = postOp.get(); // entity에 담아서
+            comment.setPost(postEntity); // comment의 post에 담기
+        } else {
+            throw new RuntimeException("없는 게시글에 댓글을 작성할 수 없습니다");
+        }
+
+        commentRepository.save(comment); // 댓글 insert
+    }
 }
--- src/main/java/site/metacoding/recipemarket/web/CommentController.java
+++ src/main/java/site/metacoding/recipemarket/web/CommentController.java
@@ -1,5 +1,28 @@
 package site.metacoding.recipemarket.web;
 
-public class CommentController {
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import lombok.RequiredArgsConstructor;
+import site.metacoding.recipemarket.config.auth.LoginUser;
+import site.metacoding.recipemarket.domain.comment.Comment;
+import site.metacoding.recipemarket.domain.user.User;
+import site.metacoding.recipemarket.service.CommentService;
+
+@RequiredArgsConstructor
+@Controller
+public class CommentController {
+    private final CommentService commentService;
+
+    // 상세보기 페이지에 댓글 등록하기
+    @PostMapping("/s/post/{postId}/comment")
+    public String write(@PathVariable Integer postId, Comment comment, @AuthenticationPrincipal LoginUser loginUser) {
+
+        User principal = loginUser.getUser(); // 시큐리티 세션에 있는 유저정보를
+        comment.setUser(principal); // comment의 유저에 담아서
+        commentService.댓글쓰기(comment, postId); // 서비스로 보내기
+        return "redirect:/post/" + postId;
+    }
 }
